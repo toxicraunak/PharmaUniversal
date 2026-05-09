bc0e263b1f0073c171b7f5a7afb41d980daea29f
Devil Boy <sahilraz9265@gmail.com>
2026-05-10 01:11:23 +0530

    feat: initialize Express server with MongoDB connection and API endpoints for configuration, categories, and products

--- api/server.js
+++ api/server.js
@@ -87,7 +87,7 @@ const distPath = path.join(__dirname, '../dist');
 
 app.use(express.static(distPath));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
